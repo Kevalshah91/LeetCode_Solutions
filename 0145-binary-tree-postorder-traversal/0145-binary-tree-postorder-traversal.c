6db0341454b1e9dc708b20eecf95acaedb12e58e
Keval Shah <145671282+Kevalshah91@users.noreply.github.com>
2024-04-13 14:46:51 +0530

    Time: 4 ms (5.80%), Space: 5.8 MB (49.80%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.c
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.c
@@ -1,27 +1,14 @@
 void post(struct TreeNode* root, int* n, int* ans)
 {
     if(root == NULL) return;
-    post(root->left, n, ans);
-    post(root->right, n, ans);
+    post(root->left,n,ans);
+    post(root->right,n,ans);
     ans[*n] = root->val;
     ++*n;
 }
-
-int countNodes(struct TreeNode* root) {
-    if (root == NULL) return 0;
-    return 1 + countNodes(root->left) + countNodes(root->right);
-}
-
 int* postorderTraversal(struct TreeNode* root, int* returnSize){
-    if (root == NULL) {
-        *returnSize = 0;
-        return NULL;
-    }
-
-    int nodeCount = countNodes(root);
-
-    int* ans = malloc(nodeCount * sizeof(int));
+    int* ans = malloc(101*sizeof(int));
     *returnSize = 0;
-    post(root, returnSize, ans);
+    post(root,returnSize,ans);
     return ans;
 }
