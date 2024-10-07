ca16e4a28398b4bd4e08f039ac1792bcf55a4235
Keval Shah <145671282+Kevalshah91@users.noreply.github.com>
2024-10-07 22:31:43 +0530

    Time: 38 ms (84.53%), Space: 16.6 MB (7.81%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
@@ -1,11 +1,13 @@
 class Solution:
     def minLength(self, s: str) -> int:
-        a = [x for x in s]
-        i = 0
-        while i < len(a) - 1:
-            if (a[i] == "A" and a[i + 1] == "B") or (a[i] == "C" and a[i + 1] == "D"):
-                del a[i:i + 2]
-                i = max(i - 1, 0)
+        a = []
+        for b in s:  
+            if not a:
+                a.append(b)
+                continue
+            if (a[-1] == 'A' and b == "B") or (a[-1] == 'C' and b == "D"):
+                a.pop()
             else:
-                i += 1
-        return (len(a))
+                a.append(b)
+        return len(a)
+        
